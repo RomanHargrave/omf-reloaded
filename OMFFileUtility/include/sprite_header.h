42774ae25a94349a69b9c78145afbffd368f258f
devezek@gmail.com <devezek@gmail.com@c67d6d4e-1435-c254-aebd-391933a9a636>
2012-03-01 20:30:05 +0000

    Added an OMF file format utility. It is written in C, and it will aid to editing and understanding file formats.

---
+++ OMFFileUtility/include/sprite_header.h
@@ -0,0 +1,37 @@
+#ifndef SPRITE_HEADER_H
+#define SPRITE_HEADER_H
+
+#include <types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct
+{
+    BYTE length;
+    short x;
+    short y;
+    WORD width;
+    WORD height;
+    BYTE index;
+    BYTE dataMissing;
+} sSpriteHeader;
+
+/**
+*   Loads a sprite header from file
+*/
+sSpriteHeader loadSpriteHeader(FILE *);
+/**
+*
+*/
+void saveSpriteHeader(sSpriteHeader,FILE *);
+/**
+*   Prints a sprite header
+*/
+void printSpriteHeader(sSpriteHeader,int offset);
+/**
+*   Gains control of the program and converts it into a sprite header editor:
+*/
+sSpriteHeader spriteHeaderEditor(sSpriteHeader);
+
+#endif // SPRITE_HEADER_H
---
+++ OMFFileUtility/src/font_8.c
@@ -0,0 +1,172 @@
+#include "font_8.h"
+
+#include <main.h>
+
+font8 loadFont8(FILE * pFile)
+{
+    // Get size:
+    int pointer = ftell(pFile);
+    int sz = fileSize(pFile);
+
+    // Restart position:
+    fseek(pFile,pointer,SEEK_SET);
+
+    // Check how much letters there are in this font:
+    sz -= pointer;
+    sz /= sizeof(font8Letter);
+
+    // Allocate memory:
+    font8 result;
+    result.f = (font8Letter*)malloc(sizeof(font8Letter)*sz);
+    result.size = sz;
+
+    // Read letters:
+    int r = fread(result.f,sizeof(font8Letter),sz,pFile);
+    if (r != sz)
+    {
+        fprintf(stderr, "Error: couldn't read a font.\n");
+        exit(0);
+    }
+    return result;
+}
+
+void saveFont8(font8 f, FILE * pFile)
+{
+    int w = fwrite(f.f,sizeof(font8Letter),f.size,pFile);
+    if (f.size != w)
+    {
+        fprintf(stderr,"Error: couldn't write the font.\n");
+        exit(0);
+    }
+}
+
+void printFont8Letter(font8Letter fl,char c, int offset)
+{
+    printf("0x%04X: %c\n",offset,c);
+    int i;
+    for (i = 0; i < 8; i++)
+    {
+        BYTE line = fl[i];
+        int j;
+        for (j = 0; j < 8; j++)
+        {
+            if (line&128)
+                printf("#");
+            else
+                printf(" ");
+            line <<= 1;
+        }
+        printf("\n");
+    }
+}
+
+void printFont8(font8 f,int offset)
+{
+    int i;
+    for (i = 0; i < f.size; i++)
+    {
+        printFont8Letter(f.f[i],32+i,offset + i*8);
+    }
+}
+
+font8 font8Editor(font8 in)
+{
+    font8 out = in;
+    char action[512];
+    while (strcmp(action,"exit"))
+    {
+        if (scanf("%s",action) == EOF)
+        {
+            printf("End of file found. Exiting.");
+            break;
+        }
+        if (!strlen(action))
+            continue;
+        if (!strcmp(action,"show"))
+        {
+            printFont8(out,0);
+        }
+        else if (!strcmp(action,"help"))
+        {
+            showCommandsHelp();
+        }
+        else if (!strcmp(action,"letters"))
+        {
+            scanf("%s",action);
+            int i;
+            for (i = 0; i < strlen(action);i++)
+                printFont8Letter(out.f[action[i]-32],action[i],(action[i]-32)*8);
+        }
+        else if (!strcmp(action,"edit"))
+        {
+            printf("Write the letters you want to edit in a single word wihtout spaces:\n");
+            printf("If you want to edit space symbol, just write \"\"\n");
+            char characters[128];
+            scanf("%s",characters);
+            printf("Now write 8 lines of 8 characters each one, where\n");
+            printf(". means blank space and # means occupied space:\n");
+            // Edit space character:
+            if (!strcmp(characters,"\"\""))
+            {
+                printf("Space:\n");
+                int i;
+                // 8 Lines:
+                for (i = 0; i < 8; i++)
+                {
+                    // Get line:
+                    scanf("%s",action);
+                    int j;
+                    // Set line to 0:
+                    BYTE line = 0;
+                    // Change 8 spaces of line:
+                    for (j = 0; j < 8; j++)
+                    {
+                        line <<= 1;
+                        line |= (action[j] == '#');
+                    }
+                    // Change line:
+                    out.f[' '-32][i] = line;
+                }
+                printf("Successful edition. Your letter:\n");
+                printFont8Letter(out.f[' '-32],' ',(' '-32)*8);
+            }
+            // Any kind of characters:
+            else
+            {
+                int k;
+                // Each character of word:
+                for (k = 0; k < strlen(characters); k++)
+                {
+                    char editting = characters[k];
+                    printf("%c:\n",editting);
+                    int i;
+                    // 8 Lines:
+                    for (i = 0; i < 8; i++)
+                    {
+                        // Get line:
+                        scanf("%s",action);
+                        // Set line to 0:
+                        BYTE line = 0;
+                        // Change 8 spaces of line:
+                        int j;
+                        for (j = 0; j < 8; j++)
+                        {
+                            line <<= 1;
+                            line |= (action[j] == '#');
+                        }
+                        // Change line:
+                        out.f[editting-32][i] = line;
+                    }
+                    printf("Successful edition. Your letter:\n");
+                    printFont8Letter(out.f[editting-32],editting,(editting-32)*8);
+                }
+            }
+        }
+        else
+        {
+            if (strcmp(action,"exit"))
+                printf("Unkown command \"%s\". Write help to check command list.\n",action);
+        }
+    }
+    return out;
+}
